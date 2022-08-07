97b4cbd1f738b7a6f9a1835569546322198fca13
Andrew Kal <andrewkalgit@gmail.com>
2022-08-06 23:09:15 -0400

    Final Demo Version for Aug 6th

--- src/Statement.js
+++ src/Statement.js
@@ -5,6 +5,7 @@ function Statement(props) {
     return (
         <div class="flex ">
         <div className='maven-text-statement'>
+            *This text will change and update by itself in the final version* <br></br>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam expedita culpa placeat in omnis consectetur praesentium consequatur rem dicta, id delectus quia reiciendis laudantium laborum? Quia odio doloremque fugit in.
         </div>
         <div className="man-making-tea-container">
