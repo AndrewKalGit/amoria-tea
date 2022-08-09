3e2b648deedd8882391198c3e71b02032e507292
Andrew Kal <andrewkalgit@gmail.com>
2022-08-09 19:16:28 -0400

    can change text but need it to loop in the statement (Move it to a component changingHeader)

--- src/Components/Statement.js
+++ src/Components/Statement.js
@@ -1,25 +1,25 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState, useEffect, useCallback } from 'react';
 import TeaMaking from '../SiteImg/man-making-tea.jpg'
 
 function Statement(props) {
-    const [toggle, setToggle] = useState(false)
+    const headingStatement = ['Natural', 'Fresh', 'Modern', 'Comfortable', 'Quality', 'Yum.'];
+
+    const [index, setIndex] = useState(0)
 
     useEffect(() => {   
-        const intervalID = setTimeout(() => {
-            setToggle((toggle) => !toggle) //Notes: Give setToggle an updater function instead of simple values.
-        }, 5000);
-
-        return () =>clearInterval(intervalID); //Notes: Clean up function: Clears the interval on component dismount. Prevents bugs as you try to update the component after dismount from DOM.
-    }, []); //Notes: Dependency array = empty; ensuring execution of useEffect only on initial mounting of the component 
+        const intervalID = setInterval(() => {
+            setIndex(index + 1)
+        }, 3400);
+        return () => clearInterval(intervalID); //Notes: Clean up function: Clears the interval on component dismount. Prevents bugs as you try to update the component after dismount from DOM.
+    },); //Notes: Dependency array = empty; ensuring execution of useEffect only on initial mounting of the component 
 
     //Attribution: https://stackoverflow.com/questions/65504562/change-text-every-3-seconds-react-useeffect
 
-    let text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi magni ipsa quaerat iste incidunt voluptatum doloremque, accusamus doloribus in eius repellendus ad sit atque id officia non laboriosam sequi voluptate!"
-
     return (
-        <div class="flex ">
+        <div class="flex">
         <div className='maven-text-statement'>
-            {text}
+          {headingStatement[index]} <br/>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi magni ipsa quaerat iste incidunt voluptatum doloremque, accusamus doloribus in eius repellendus ad sit atque id officia non laboriosam sequi voluptate!
         </div>
         <div className="man-making-tea-container">
             <img className='man-making-tea-img' src={TeaMaking} alt="man making tea"/>
