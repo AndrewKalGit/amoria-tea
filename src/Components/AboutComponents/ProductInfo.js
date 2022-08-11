61f263b2e742e070b4696ee48bfcec3598a03890
Andrew Kal <andrewkalgit@gmail.com>
2022-08-11 12:56:10 -0400

    fixed about page UI negative margin added to space out divs and imgs evenly

--- src/Components/AboutComponents/ProductInfo.js
+++ src/Components/AboutComponents/ProductInfo.js
@@ -6,11 +6,11 @@ function ProductInfo(props) {
         <div class='flex'>
         <div className="about-container-comic">
         <div class='flex flex-col sm:flex-row-reverse'>
-        <div class="ml-1 text-sm sm:text-xl md:text-2xl lg:text-4xl">
+        <div class="-mt-4 ml-1 text-sm sm:text-xl md:text-2xl lg:text-4xl">
         <br/>
         <strong className='poppins-bolded-responsive'> No. Powders. Ever </strong> 
         <br/>
-        <p class="text-sm sm:text-xl md:text-2xl lg:text-4xl lg:mt-4 mb-2">
+        <p class="text-sm sm:text-xl md:text-2xl lg:text-4xl lg:mt-4 mb-1">
         We worked hard to create premium tea blends that pair perfectly with freshly made fruit purees and quality milks.
         </p>
         </div>
